cec2a27dff404add5393dc85bfe892b29b748aa9
zuchie <zuchie@gmail.com>
2014-05-31 17:04:20 -0700

    3rd, add new files and make changes

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Rango says hello yo! <a href='/rango/about'>About</a>")
+
+def about(request):
+    return HttpResponse("This is about page. <a href='/rango/index'>Index</a>")
+
