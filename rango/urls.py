992c604533a7e7618fada44ad7741caaa6cd9cab
zuchie <zuchie@gmail.com>
2014-07-07 02:18:33 -0700

    7th commit, adding add_page form

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^about/$', views.about, name='about'),
 	url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
+        #url(r'^add_page/$', views.add_page, name='add_page'), # NEW MAPPING!
+        url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'), # NEW MAPPING!
         url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),
 )
--- rango/views.py
+++ rango/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Category, Page
 from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def category(request, category_name_url):
     context = RequestContext(request)
@@ -10,6 +11,7 @@ def category(request, category_name_url):
     context_dict = {'category_name': category_name}
     try:
         category = Category.objects.get(name=category_name)
+        category.url = category_name_url
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
         context_dict['category'] = category
@@ -83,4 +85,46 @@ def add_category(request):
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render_to_response('rango/add_category.html', {'form': form}, context)
+
+def add_page(request, category_name_url):
+    context = RequestContext(request)
+
+    #category.url = category_name_url
+    #category_name = decode_url(category_name_url)
+    category_name = category_name_url.replace('_', ' ')
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            # This time we cannot commit straight away.
+            # Not all fields are automatically populated!
+            page = form.save(commit=False)
+
+            # Retrieve the associated Category object so we can add it.
+            # Wrap the code in a try block - check if the category actually exists!
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.category = cat
+            except Category.DoesNotExist:
+                # If we get here, the category does not exist.
+                # Go back and render the add category form as a way of saying the category does not exist.
+                return render_to_response('rango/add_category.html', {}, context)
+
+            # Also, create a default value for the number of views.
+            page.views = 0
+
+            # With this, we can then save our new model instance.
+            page.save()
+
+            # Now that the page is saved, display the category instead.
+            return category(request, category_name_url)
+        else:
+            print form.errors
+    else:
+        form = PageForm()
+
+    return render_to_response( 'rango/add_page.html',
+            {'category_name_url': category_name_url,
+             'category_name': category_name, 'form': form},
+             context)
 
