07aceaa554572c70c3d4fd716e69edd21b081087
zuchie <zuchie@gmail.com>
2014-11-27 09:03:48 -0800

    put translate into home page with inclusion_tag and custom template filter technique, working on modify

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from rango.models import Dict 
 from rango.forms import DictForm
+from django.template.defaulttags import register
 
 def index(request):
     # Request the context of the request.
@@ -12,23 +13,25 @@ def index(request):
     # A HTTP POST?
     if request.method == 'POST':
         form = DictForm(request.POST)
-        # A new dic form to be used later.
-        final_form = DictForm()
+        trans_form = DictForm()
 
         # Have we been provided with a valid form 'text' field?
         if form.data['text']:
             txt = form.data['text']
             # Is provided 'text' already in dictionary?
             if Dict.objects.filter(text = txt).exists() == True:
-                dic = Dict.objects.get(text = txt)
-                # Pass field data to new dic form, since form is immutable.
-                final_form.data['text'] = txt 
-                final_form.data['translation'] = dic.translation
-                # Render field data to html.
-                return render_to_response('rango/translate.html', {'form': final_form}, context)
+                trans = Dict.objects.get(text = txt).translation
+                trans_form.data['text'] = txt
+                trans_form.data['translation'] = trans
+                return render_to_response('rango/index.html', {'form': form, 'trans_form': trans_form}, context)
+#                dict_template = 'rango/translate.html'
+#                my_dict = Dict.objects.get(text = txt) 
+#                return render_to_response(dict_template, {'dict': my_dict}, context)
             # Text not in dic, add it into dic.
             else: 
-                return render_to_response('rango/add_item.html', {'form': form}, context)
+                dict_template = 'rango/add_item.html'
+                dict_form = form
+                return render_to_response(dict_template, {'form': dict_form}, context)
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
@@ -39,6 +42,7 @@ def index(request):
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
     return render_to_response('rango/index.html', {'form': form}, context)
+
 
 def add_item(request):
     # Get the context from the request.
@@ -70,6 +74,7 @@ def modify_item(request):
     # Get the context from the request.
     context = RequestContext(request)
 
+    print request.GET.__dict__
     # A HTTP GET?
     if request.method == 'GET':
         form = DictForm(request.GET)
@@ -84,6 +89,12 @@ def modify_item(request):
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
+    # POST
+    elif request.method == 'POST':
+        form = DictForm(request.GET)
+        print form.data
+        if form.is_valid():
+            return render_to_response('rango/modify_item.html', {'form': form}, context)
     else:
         # If the request was not a POST, display the form to enter details.
         form = DictForm()
