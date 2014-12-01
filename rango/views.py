eceb177129617f524fb691c5e19e670c71d22d17
zuchie <zuchie@gmail.com>
2014-11-30 19:27:12 -0800

    1. passing data among views by session variable, solved modify item issue. 2. use POST.copy() to make request.POST/DictForm mutable in order to using single form to make changes to DB

--- rango/views.py
+++ rango/views.py
@@ -12,37 +12,50 @@ def index(request):
     context = RequestContext(request)
     # A HTTP POST?
     if request.method == 'POST':
-        form = DictForm(request.POST)
+        form = DictForm()
+    else:
+        form = DictForm(request.GET)
         trans_form = DictForm()
 
         # Have we been provided with a valid form 'text' field?
 #        if form.data['text']:
-        if 'translate' in request.POST:
+        if 'translate' in request.GET:
+            request.session['_old_get'] = request.GET
             txt = form.data['text']
-            # Is provided 'text' already in dictionary?
-            if Dict.objects.filter(text = txt).exists() == True:
-                trans = Dict.objects.get(text = txt).translation
-                trans_form.data['text'] = txt
-                trans_form.data['translation'] = trans
-                print trans_form.data['translation']
-                return render_to_response('rango/index.html', {'form': form, 'trans_form': trans_form}, context)
+            # Is it a blank input?
+            if txt: 
+                # Is provided 'text' already in dictionary?
+                if Dict.objects.filter(text = txt).exists() == True:
+                    trans = Dict.objects.get(text = txt).translation
+                    trans_form.data['text'] = txt
+                    trans_form.data['translation'] = trans
+                    return render_to_response('rango/index.html', {'form': form, 'trans_form': trans_form}, context)
 #                dict_template = 'rango/translate.html'
 #                my_dict = Dict.objects.get(text = txt) 
 #                return render_to_response(dict_template, {'dict': my_dict}, context)
             # Text not in dic, add it into dic.
-            else: 
-                dict_template = 'rango/add_item.html'
-                dict_form = form
-                return render_to_response(dict_template, {'form': dict_form}, context)
-        elif 'modify' in request.POST:
+                else: 
+                    dict_template = 'rango/add_item.html'
+                    dict_form = form
+                    return render_to_response(dict_template, {'form': dict_form}, context)
+            # a blank input
+            else:
+                form = DictForm()
+
+        elif 'modify' in request.GET:
+            old_get = request.session.get('_old_get')
+            txt = old_get['text']
+            trans = Dict.objects.get(text = txt).translation
+            trans_form.data['text'] = txt 
+            trans_form.data['translation'] = trans 
             dict_template = 'rango/modify_item.html'
-            return render_to_response(dict_template, {'form': form}, context)
+            return render_to_response(dict_template, {'form': trans_form}, context)
+
         else:
             # The supplied form contained errors - just print them to the terminal.
-            print form.errors
-    else:
+#            print form.errors
         # If the request was not a POST, display the form to enter details.
-        form = DictForm()
+            form = DictForm()
 
     # Bad form (or form details), no form supplied...
     # Render the form with error messages (if any).
@@ -79,13 +92,24 @@ def modify_item(request):
     # Get the context from the request.
     context = RequestContext(request)
 
-    # A HTTP GET?
-    if request.method == 'GET':
-        form = DictForm(request.GET)
+    # A HTTP POST?
+    if request.method == 'POST':
+        # Get text field value from old session.
+        old_get = request.session.get('_old_get')
+        txt = old_get['text']
+        # make request.POST mutable
+        mutable_req_POST = request.POST.copy()
+        # Get new form from POST.
+        form = DictForm(mutable_req_POST)
+        # Get new translation value from new form.
+        trans = form['translation'].value()
         # Have we been provided with a valid form?
-        if form.is_valid():
+#        if form.is_valid():
+        if Dict.objects.filter(text = txt).exists() == True:
+            form.data['text'] = txt
+            form.data['translation'] = trans
             # Delete original.
-            Dict.objects.filter(text = form.data['text']).delete()
+            Dict.objects.filter(text = txt).delete()
             # Save new.
             form.save(commit=True)
             # Return home
@@ -93,8 +117,8 @@ def modify_item(request):
         else:
             # The supplied form contained errors - just print them to the terminal.
             print form.errors
-    # POST
-    elif request.method == 'POST':
+    # GET 
+    elif request.method == 'GET':
         form = DictForm(request.GET)
         print form.data
         if form.is_valid():
