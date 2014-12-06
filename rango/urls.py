f3f5660168f6242370d4f85ef7811b7d8f4bbe74
zuchie <zuchie@gmail.com>
2014-12-06 00:14:24 -0800

    use URL to pass template variables to views, instead of using session variable

--- rango/urls.py
+++ rango/urls.py
@@ -3,6 +3,7 @@ from rango import views
 
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
-        url(r'^add_item/$', views.add_item, name='add_item'),
-        url(r'^modify_item/$', views.modify_item, name='modify_item'),
+        url(r'^translate/$', views.translate_item, name='translate_item'),
+        url(r'^add/(?P<input_text>.+)/$', views.add_item, name='add_item'),
+        url(r'^modify/(?P<input_text>.+)/$', views.modify_item, name='modify_item'),
 )
--- rango/views.py
+++ rango/views.py
@@ -12,49 +12,59 @@ def index(request):
     context = RequestContext(request)
     # A HTTP POST?
     if request.method == 'POST':
+        form = DictForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit = True)
+            return index(request)
+
+        else:
+            print form.errors
+
+    else:
+        form = DictForm()
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
+    return render_to_response('rango/index.html', {'form': form}, context)
+
+def translate_item(request):
+    # Request the context of the request.
+    # The context contains information such as the client's machine details, for example.
+    context = RequestContext(request)
+    # A HTTP POST?
+    if request.method == 'POST':
         form = DictForm()
     # GET
     else:
-        # Make form mutable.
-        mutable_req_POST = request.GET.copy()
-        form = DictForm(mutable_req_POST)
+        # Get user request.
+        req = request.GET
+        form = DictForm(req)
+        response_form = DictForm()
 
         # Has 'translate' button been clicked?
-        if 'translate' in request.GET:
-            # Save mutable request.GET for other views.
-            request.session['_old_get'] = mutable_req_POST
+        if 'translate' in req:
             # Get text from user input and strip leading/trailing whitespaces.
-            text_input = form.data['text'].strip()
+            txt = form.data['text'].strip()
             # Is it a blank input?
-            if text_input: 
+            if txt: 
                 # Is provided 'text' already in dictionary?
-                if Dict.objects.filter(text = text_input).exists() == True:
-                    form.data['translation'] = Dict.objects.get(text = text_input).translation
+                if Dict.objects.filter(text = txt).exists() == True:
+                    trans = Dict.objects.get(text = txt).translation
+                    response_form.data['text'] = txt
+                    response_form.data['translation'] = trans 
                     template = 'rango/index.html'
                 # Text not in dic, add it into dic.
                 else: 
-                    form.data['text'] = form.data['text'].strip()
+                    response_form.data['text'] = txt
                     template = 'rango/add_item.html'
 
-                return render_to_response(template, {'form': form}, context)
+                return render_to_response(template, {'form': response_form}, context)
             # A blank input, show form.
             else:
                 form = DictForm()
 
-        # Has 'modify' button been clicked?
-        elif 'modify' in request.GET:
-            # Get text.
-            old_get = request.session.get('_old_get')
-            text_input = old_get['text'].strip()
-            form.data['text'] = text_input 
-            form.data['translation'] = Dict.objects.get(text = text_input).translation
-            template = 'rango/modify_item.html'
-            return render_to_response(template, {'form': form}, context)
-
         else:
-            # The supplied form contained errors - just print them to the terminal.
-#            print form.errors
-        # If the request was not a POST, display the form to enter details.
+            # Not a translate req, display the form to enter details.
             form = DictForm()
 
     # Bad form (or form details), no form supplied...
@@ -62,14 +72,14 @@ def index(request):
     return render_to_response('rango/index.html', {'form': form}, context)
 
 
-def add_item(request):
+def add_item(request, input_text):
     # Get the context from the request.
     context = RequestContext(request)
 
     # A HTTP POST?
     if request.method == 'POST':
-        form = DictForm(request.POST)
-
+        form = DictForm(request.POST.copy())
+        form.data['text'] = input_text
         # Have we been provided with a valid form?
         if form.is_valid():
             # Save the new category to the database.
@@ -88,35 +98,51 @@ def add_item(request):
     # Render the form with error messages (if any).
     return render_to_response('rango/index.html', {'form': form}, context)
 
-def modify_item(request):
+def modify_item(request, input_text):
     # Get the context from the request.
     context = RequestContext(request)
 
     # A HTTP POST?
     if request.method == 'POST':
-        # Get text field value from old session.
-        old_get = request.session.get('_old_get')
-        txt = old_get['text']
-        # make request.POST mutable
-        mutable_req_POST = request.POST.copy()
-        # Get new form from POST.
-        form = DictForm(mutable_req_POST)
-        # Get new translation value from new form.
-        trans = form['translation'].value()
-        # Have we been provided with a valid form?
-#        if form.is_valid():
-        if Dict.objects.filter(text = txt).exists() == True:
-            form.data['text'] = txt
-            form.data['translation'] = trans.strip()
-            # Delete original.
-            Dict.objects.filter(text = txt).delete()
-            # Save new.
-            form.save(commit=True)
-            # Return home
-            return HttpResponseRedirect('/rango/')
+        req = request.POST
+        form = DictForm(req)
+        trans = input_text
+        if Dict.objects.filter(translation = trans).exists() == True:
+            txt = Dict.objects.get(translation = trans).text
+ 
+        # Has 'modify' button been clicked?
+        if 'modify' in req:
+            print 'OK'
+            response_form = DictForm()
+            print txt, trans
+            # Is it a blank input?
+            if txt and trans: 
+                response_form.data['text'] = txt
+                response_form.data['translation'] = trans 
+                template = 'rango/modify_item.html'
+                return render_to_response(template, {'form': response_form}, context)
+            # A blank input, show form.
+            else:
+                form = DictForm()
+        elif 'submit' in req:
+            final_form = DictForm(req.copy())
+            print final_form.data['translation']
+            print form.is_valid()
+            if input_text:
+                final_form.data['text'] = input_text 
+                print final_form.data['text']
+                print final_form.data['translation']
+                # Delete original.
+                Dict.objects.filter(text = input_text).delete()
+                # Save new.
+                final_form.save(commit=True)
+                # Return home
+                return HttpResponseRedirect('/rango/')
+
         else:
-            # The supplied form contained errors - just print them to the terminal.
-            print form.errors
+            # Not a translate req, display the form to enter details.
+            form = DictForm()
+
     else:
         # If the request was not a POST, display the form to enter details.
         form = DictForm()
